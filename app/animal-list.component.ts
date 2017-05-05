b51c37f38e532a0be3b917f0501ce498b667c58c
Cameron Jensen <jensenseanc@gmail.com>
2017-05-05 10:29:28 -0700

    add animal-list component

---
+++ app/animal-list.component.ts
@@ -0,0 +1,13 @@
+import { Componenet, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './zoo-animals.model';
+
+@Componenet({
+  selector: `animal-list`,
+  template: `
+  
+  `
+})
+
+export class AnimalListComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,18 @@ import { Animal } from './zoo-animals.model';
 @Component ({
   selector: 'app-root',
   template: `
-
+  <div class="container">
+    <div class="row">
+      <p>this is the app-root page</p>
+      <animal-list></animal-list>
+    </div>
+  </div>
   `
 })
 
 export class AppComponent {
-  
+  allAnimals: Animal[] = [
+    new Animal("Tiger", "Carnivore", "Cat", "Kitty", 2, "meat", "Siberia", 4, "Male", "beef", "carrots")
+  ];
+  selectedAnimal = null;
 }
