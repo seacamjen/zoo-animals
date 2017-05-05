e90dfc3d1feeffd760f9a7ba48da5d4fcb819be6
Cameron Jensen <jensenseanc@gmail.com>
2017-05-05 08:55:19 -0700

    add app compnent with basic info

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Animal } from './zoo-animals.model';
+
+@Component ({
+  selector: 'app-root',
+  template: `
+
+  `
+})
+
+export class AppComponent {
+  
+}
